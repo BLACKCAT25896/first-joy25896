1cfacc192f0c3541217d9d80826f9da232c9ae49
BLACKCAT25896 <joykamrujjaman@gmail.com>
2017-11-12 23:47:47 +0600

    dd

--- deploy/settings.py
+++ deploy/settings.py
@@ -132,10 +132,7 @@ USE_TZ = True
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
-DATABASES = { 'default': dj_database_url.config() }
-
-
-
+DATABASES = { 'default': dj_database_url.config(conn_max_age=500) }
 
 STATIC_URL = '/static/'
 

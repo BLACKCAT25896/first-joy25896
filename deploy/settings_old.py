1c0b0d7c58c3598e265f5166f9ddbe80eebd9c20
BLACKCAT25896 <joykamrujjaman@gmail.com>
2017-11-13 23:12:51 +0600

    dd

--- deploy/settings_old.py
+++ deploy/settings_old.py
@@ -151,6 +151,11 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
 
 
+# PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+# STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT,'static/'),
+# )
 
 
 
